f7f1f16e195cdebc22c935e4cede510657b9de7e
Peter <peterrose03@gmail.com>
2021-09-23 19:09:18 +0200

    linux_fx_version = "DOTNETCORE|v5.0"

--- Artemis-Terraform/main.tf
+++ Artemis-Terraform/main.tf
@@ -62,7 +62,7 @@ resource "azurerm_app_service" "artemis" {
 
     site_config {
         dotnet_framework_version = "v5.0"
-        linux_fx_version = "v5.0"
+        linux_fx_version = "DOTNETCORE|v5.0"
         # remote_debugging_enabled = true
         # remote_debugging_version = "VS2019"
         always_on = "true"
@@ -112,6 +112,7 @@ resource "azurerm_app_service_slot" "artemis-slot" {
 
     site_config {
         dotnet_framework_version = "v5.0"
+        linux_fx_version = "DOTNETCORE|v5.0"
         # remote_debugging_enabled = true
         # remote_debugging_version = "VS2019"
         always_on = "true"
